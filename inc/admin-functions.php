2bd072edbf34ba718a0bea4c1b5c73d6f5923fdb
osdal <osdal67@gmail.com>
2023-03-09 10:09:35 +0300

    меню сайта

--- inc/admin-functions.php
+++ inc/admin-functions.php
@@ -1,27 +1,42 @@
 <?php
 function add_additional_admin_page(){
-	add_menu_page( 'Дополнительные настройки сайта', 
-							'Дополнительные настройки', 
-							'edit_others_posts', 
-							'additional-options', 
-							'create_additional_admin_page', 
-							get_template_directory_uri() . '/assets/img/Logo_ico.png' );
+	$hook_suffix = add_menu_page( 'Дополнительные настройки сайта', 
+						'Дополнительные настройки', 
+						'edit_others_posts', 
+						'additional-options', 
+						'create_additional_admin_page', 
+						get_template_directory_uri() . '/assets/img/Logo_ico.png',
+	);
+	add_submenu_page( 'additional-options', 'Настройки платежных реквизитов', 'Платежные реквизиты', 'edit_others_posts', 'additional-options-banks', 'create_additional_admin_page_banks', );
+	add_submenu_page( 'additional-options', 'Настройки контактов', 'Контакты', 'edit_others_posts', 'additional-options-social', 'create_additional_admin_page_social', 10);
 
-	add_action('admin_enqueue_scripts', 'add_admin_style');
+
+	add_action("admin_print_scripts-{$hook_suffix}", "add_admin_style");//подключение файла стилей к конкретной странице
+	// add_action('admin_enqueue_scripts', 'add_admin_style');подключение файла стилей ко всем страницам админки
 }
-
 
 add_action( 'admin_menu', 'add_additional_admin_page', 85 );
 
+add_action('admin_init', 'custom_settings');
+
+function custom_settings(){
+	register_setting( 'contacts_group', 'instagram');
+
+	add_settings_section( 'contacts_group_section', 'Мессенджеры', '', 'additional-options-social');
+}
+
+
 function create_additional_admin_page() {
-	?>
-	<div class="wrap">
-		<h1>Дополнительные настройки сайта</h1>
-	</div>
+	require get_template_directory() . '/inc/templates/aditional-options.php';
+	
+}
 
+function create_additional_admin_page_social(){
+	require get_template_directory() . '/inc/templates/social.php';
+}
 
-
-	<?php
+function create_additional_admin_page_banks(){
+	require get_template_directory() . '/inc/templates/banks.php';
 }
 
 function add_admin_style(){
---
+++ inc/templates/aditional-options.php
@@ -0,0 +1,10 @@
+<div class="wrap">
+		<h1 title>Дополнительные настройки сайта</h1>
+
+		<ol class="opt">
+			<li><a href="<?php echo get_site_url() ?>/wp-admin/admin.php?page=additional-options-banks">Платежные реквизиты</a></li>
+			<li><a href="<?php echo get_site_url() ?>/wp-admin/admin.php?page=additional-options-social">Контакты</a></li>
+		</ol>
+
+		
+</div>
